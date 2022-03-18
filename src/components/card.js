d4987f537936164e25e5a2089b298a8f10498e92
unverzed <vitoriarortega@gmail.com>
2022-03-18 19:13:22 -0300

    feat: inserting usestate to flip cards (not working)

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,36 @@
-export default function Card(){
+import {useState} from "react"
+
+
+export default function Card({questionNumber, question, answer}){
+    const [step, setStep] = useState("start-page")
+
+    if(step === "start-page") {
     return (
         <div className="all-cards">
             <div className="card">
-            <p>Card teste</p>
-            <img src="./assets/vector.png"/>
+            <p>Pergunta {questionNumber}</p>
+            <ion-icon name="play-outline" onClick={() => setStep("front-page")}></ion-icon>
             </div>
         </div>
     )
+   }
+
+    else if (step === "front-page") {
+        return (
+            <div className="front-page">
+            <p>{question}</p>
+            <img className="./assets/setinha.png" onClick={() => setStep("back-face")}/>
+            </div>
+        )
+    }
+
+    else if (step === "back-face") {
+        return (
+            <div className="all-cards">
+            <p>{answer}</p>
+            </div>
+        )
+    }
+
 }
+
--- src/components/cardcontent.js
+++ src/components/cardcontent.js
@@ -1,37 +1,55 @@
-export const text = [{
+import Card from "./card";
+
+export default function DeckContent() {
+     
+  const text = [{
+        questionNumber: "Pergunta 1",
         question: "O que é JSX?",
         answer: "O JSX é uma extensão de sintaxe para JavaScript, que pode lembrar uma linguagem de template, mas que vem com todo o poder do JavaScript.",
       },
-      {
+      { 
+        questionNumber: "Pergunta 2",
         question: "O React é __",
         answer: "Uma biblioteca JavaScript para construção de interfaces.",
        
       },
       {
+        questionNumber: "Pergunta 3",
         question: "Componentes devem iniciar com __",
         answer: "Letra maiúscula.",
       
       },
       {
+        questionNumber: "Pergunta 4",
         question: "Podemos colocar __ dentro do JSX ",
         answer: "Expressões.",
       
       },
       {
+        questionNumber: "Pergunta 5",
         question: "O ReactDOM nos ajuda __ ",
         answer: "Interagindo com a DOM para colocar componentes React na mesma.",
       },
-      {
+      { 
+        questionNumber: "Pergunta 6",
         question: "Usamos o npm para __",
         answer: "Gerenciar os pacotes necessários e suas dependências.",
         
       },
-      {
+      { questionNumber: "Pergunta 7",
         question: "Usamos props para __",
         answer: "Passar diferentes informações para componentes.",
        
       },
-      {
+      { questionNumber: "Pergunta 8",
         question: "Usamos estado (state) para __",
         answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente.",   
       }];
+
+      return (
+        <div className="front-card">
+        {text.map(content => <Card questionNumber={content.questionNumber} question={content.question} answer={content.answer}/>)}          
+        </div>
+      )
+
+  }
